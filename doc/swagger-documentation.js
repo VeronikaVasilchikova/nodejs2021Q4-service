b806be7c175e464b1cf721ae972713954ca597a3
Veronika <v.vasilchykava@gmail.com>
2021-11-30 21:11:42 +0300

    init: start REST implementation using Hapi

---
+++ doc/swagger-documentation.js
@@ -0,0 +1,11 @@
+const swaggerOptions = {
+  jsonPath: '/documentation.json',
+  documentationPath: '/documentation',
+  info: {
+    title: 'Trello Service',
+    description: `Let's try to create a competitor for Trello!`,
+    version: '1.0.0'
+  }
+};
+
+module.exports = swaggerOptions;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,32 @@
+const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const HapiSwagger = require('hapi-swagger');
 const { PORT } = require('./common/config');
-const app = require('./app');
+const swaggerOptions = require('../doc/swagger-documentation');
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+const init = async () => {
+  const server = Hapi.server({
+    port: PORT || 3000,
+    host: 'localhost'
+  });
+
+  await server.register([
+    Inert,
+    Vision,
+    {
+      plugin: HapiSwagger,
+      options: swaggerOptions
+    }
+  ]);
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();

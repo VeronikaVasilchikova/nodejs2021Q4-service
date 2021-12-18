a636a78953c710d5846d1c5772ea73d8b605c577
Veronika <v.vasilchykava@gmail.com>
2021-12-18 12:43:27 +0300

    refactor: update user.service

--- src/resources/users/user.service.ts
+++ src/resources/users/user.service.ts
@@ -1,43 +1,96 @@
+import * as Hapi from '@hapi/hapi';
+import { Request } from "@hapi/hapi";
+import Boom from '@hapi/boom';
 import UserMemoryRepository from './user.memory.repository';
 import TaskMemoryRepository from '../tasks/task.memory.repository';
 import {IUserData, ICreatedUserData} from '../helpers/interfaces';
+import User from './user.model';
 
 export default class UserService {
   /**
-   * Returns all users
-   * @returns Promise resolved user array
+   * Returns Hapi response with all users
+   * @param request Hapi request
+   * @param h Hapi response
+   * @returns Promise resolved Hapi response object
    */
-  public static getAllUsers = (): Promise<Array<IUserData> | []> => UserMemoryRepository.getAllUsers();
+  public static getAllUsers = async (request: Request, h: Hapi.ResponseToolkit): Promise<Hapi.ResponseObject> => {
+    try {
+      const allUsers = await UserMemoryRepository.getAllUsers();
+      const res = allUsers.length ? allUsers.map(User.toResponse.bind(User)) : [];
+      return h.response(res).code(200);
+    }
+    catch (error) {
+      throw Boom.badImplementation((<Error>error).message);
+    }
+  }
 
   /**
-   * Returns an existing user based on identifier
-   * @param userId user identifier
-   * @returns Promise resolved user data
+   * Returns Hapi response with user data
+   * @param request Hapi request
+   * @param h Hapi response
+   * @returns Promise resolved Hapi response object
    */
-  public static getUserById = (userId: string): Promise<IUserData | undefined> => UserMemoryRepository.getUserById(userId);
+  public static getUserById = async (request: Request, h: Hapi.ResponseToolkit): Promise<Hapi.ResponseObject> => {
+    const {userId} = request.params;
+    try {
+      const user = await UserMemoryRepository.getUserById(<string>userId)
+      return h.response(User.toResponse(<IUserData>user)).code(200);
+    }
+    catch (error) {
+      throw Boom.badImplementation((<Error>error).message);
+    }
+  }
 
   /**
-   * Returns an updated user based on identifier
-   * @param userId user identifier
-   * @param data new user data
-   * @returns Promise resolved an updated user data
+   * Returns Hapi response with updated user data
+   * @param request Hapi request
+   * @param h Hapi response
+   * @returns Promise resolved Hapi response object
    */
-  public static updateUserById = (userId: string, data: IUserData): Promise<IUserData> => UserMemoryRepository.updateUserById(userId, data);
+  public static updateUserById = async (request: Request, h: Hapi.ResponseToolkit): Promise<Hapi.ResponseObject> => {
+    try {
+      const payload: IUserData = <IUserData>request.payload;
+      const {userId} = request.params;
+      const updatedUser: IUserData = await UserMemoryRepository.updateUserById(<string>userId, payload);
+      return h.response(User.toResponse(updatedUser)).code(200);
+    }
+    catch (error) {
+      throw Boom.badImplementation((<Error>error).message);
+    }
+  }
 
   /**
-   * Returns a newly created user data
-   * @param user new user data
-   * @returns Promise resolved a newly created user data
+   * Returns Hapi response with newly created user data
+   * @param request Hapi request
+   * @param h Hapi response
+   * @returns Promise resolved Hapi response object
    */
-  public static createUser = (user: ICreatedUserData): Promise<IUserData> => UserMemoryRepository.createUser(user);
+  public static createUser = async (request: Request, h: Hapi.ResponseToolkit): Promise<Hapi.ResponseObject> => {
+    try {
+      const payload: ICreatedUserData = <ICreatedUserData>request.payload;
+      const createdUser = await UserMemoryRepository.createUser(payload);
+      return h.response(User.toResponse(createdUser)).code(201);
+    }
+    catch (error) {
+      throw Boom.badImplementation((<Error>error).message);
+    }
+  }
 
   /**
-   * Remove an existing user from database based on user identifier
-   * @param userId identifier of user
-   * @returns Promise resolved no data
+   * Returns Hapi response with message about deleted user
+   * @param request Hapi request
+   * @param h Hapi response
+   * @returns Promise resolved Hapi response object
    */
-  public static removeUserById = (userId: string): void => {
-    TaskMemoryRepository.updateTaskByUserId(userId);
-    UserMemoryRepository.removeUserById(userId);
-  };
+  public static removeUserById = async (request: Request, h: Hapi.ResponseToolkit): Promise<Hapi.ResponseObject> => {
+    try {
+      const {userId} = request.params;
+      await TaskMemoryRepository.updateTaskByUserId(<string>userId);
+      await UserMemoryRepository.removeUserById(<string>userId);
+      return h.response('The user has been deleted').code(204);
+    }
+    catch (error) {
+      throw Boom.badImplementation((<Error>error).message);
+    }
+  }
 }

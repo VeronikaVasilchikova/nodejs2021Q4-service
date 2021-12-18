e1eb29bb58275634295bcd62d9f86ae5a4a3272b
Veronika <v.vasilchykava@gmail.com>
2021-12-18 12:51:26 +0300

    refactor: update board.router.options

--- src/resources/boards/board.router.options.ts
+++ src/resources/boards/board.router.options.ts
@@ -1,18 +1,10 @@
-import * as Hapi from '@hapi/hapi';
-import { Request } from "@hapi/hapi";
 import Joi from 'joi';
-import Boom from '@hapi/boom';
 import BoardService from './board.service';
-import TaskService from '../tasks/task.service';
 import boardSchema from './board.schema';
-import { IBoardData, IBoardDataBasic } from '../helpers/interfaces';
 
 const boardRouterOptions = {
   getAllBoards: {
-    handler: async (request: Request, h: Hapi.ResponseToolkit): Promise<Hapi.ResponseObject> => {
-      const allBoards = await BoardService.getAllBoards();
-      return h.response(allBoards).code(200);
-    },
+    handler: BoardService.getAllBoards,
     plugins: {
       'hapi-swagger': {
         responses: {
@@ -34,12 +26,7 @@ const boardRouterOptions = {
     }
   },
   getBoard: {
-    handler: async (request: Request, h: Hapi.ResponseToolkit): Promise<Hapi.ResponseObject> => {
-      const {boardId} = request.params;
-      const board = await BoardService.getBoardById(<string>boardId);
-      if (!board) throw Boom.notFound('Board not found');
-      return h.response(board).code(200);
-    },
+    handler: BoardService.getBoardById,
     plugins: {
       'hapi-swagger': {
         responses: {
@@ -69,12 +56,7 @@ const boardRouterOptions = {
     }
   },
   updateBoard: {
-    handler: async (request: Request, h: Hapi.ResponseToolkit): Promise<Hapi.ResponseObject> => {
-      const payload: IBoardData = <IBoardData>request.payload;
-      const {boardId} = request.params;
-      const updatedBoard: IBoardData = await BoardService.updateBoardById(<string>boardId, payload);
-      return h.response(updatedBoard).code(200);
-    },
+    handler: BoardService.updateBoardById,
     plugins: {
       'hapi-swagger': {
         responses: {
@@ -105,11 +87,7 @@ const boardRouterOptions = {
     }
   },
   createBoard: {
-    handler: async (request: Request, h: Hapi.ResponseToolkit): Promise<Hapi.ResponseObject> => {
-      const payload: IBoardDataBasic = <IBoardDataBasic>request.payload;
-      const createdBoard = await BoardService.createBoard(payload);
-      return h.response(createdBoard).code(201);
-    },
+    handler: BoardService.createBoard,
     plugins: {
       'hapi-swagger': {
         responses: {
@@ -137,12 +115,7 @@ const boardRouterOptions = {
     }
   },
   deleteBoard: {
-    handler: async (request: Request, h: Hapi.ResponseToolkit): Promise<Hapi.ResponseObject> => {
-      const {boardId} = request.params;
-      await BoardService.removeBoardById(<string>boardId);
-      await TaskService.removeTaskById(<string>boardId);
-      return h.response('The board has been deleted').code(204);
-    },
+    handler: BoardService.removeBoardById,
     plugins: {
       'hapi-swagger': {
         responses: {

9092d09e3ef4035e1c7d2f90c3bddeca6f265f7f
Veronika <v.vasilchykava@gmail.com>
2021-12-24 21:28:59 +0300

    fix: resolve env issue for built folder

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,16 +1,18 @@
 import dotenv from 'dotenv';
-import * as path from 'path';
+import path from 'path';
+
+const pathToEnv: string = process.argv[2] === 'production' ? '../../../.env' : '../../.env';
 
 dotenv.config({
-  path: path.join(__dirname, '../../.env')
+  path: path.join(__dirname, pathToEnv)
 });
 
 const CONFIG = {
-  PORT: process.env.PORT,
-  NODE_ENV: process.env.NODE_ENV,
-  MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING,
-  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
-  AUTH_MODE: process.env.AUTH_MODE === 'true'
+  PORT: process.env['PORT'],
+  NODE_ENV: process.env['NODE_ENV'],
+  MONGO_CONNECTION_STRING: process.env['MONGO_CONNECTION_STRING'],
+  JWT_SECRET_KEY: process.env['JWT_SECRET_KEY'],
+  AUTH_MODE: process.env['AUTH_MODE'] === 'true'
 };
 
 export default CONFIG;

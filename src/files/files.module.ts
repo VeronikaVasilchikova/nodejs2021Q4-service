615fe2533974f7ff23d8afd4f43156b52ec0ad64
Veronika <v.vasilchykava@gmail.com>
2022-02-07 01:09:43 +0300

    chore: remove unnecessary comments

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -9,23 +9,3 @@ import { AuthModule } from '../auth/auth.module';
   controllers: [FilesController],
 })
 export class FilesModule {}
-
-// import { forwardRef, Module } from '@nestjs/common';
-// import { FileService } from './file.service';
-// import { ConfigDinamicModule } from './config-dinamic.module';
-// import { ConfigModule } from '@nestjs/config';
-// import { AuthModule } from 'src/auth/auth.module';
-
-// @Module({
-//   imports: [
-//     forwardRef(() => AuthModule),
-//     ConfigModule.forRoot({
-//       envFilePath: `.${process.env.NODE_ENV}.env`,
-//     }),
-//   ],
-//   controllers: [
-//     new ConfigDinamicModule(process.env.USE_FASTIFY).controllerFile(),
-//   ],
-//   providers: [FileService],
-// })
-// export class FileModule {}
